aa438f52e0a8041398b16a38f3cf86a002fb3a59
Thiago Torres de Oliveira <osuperthiago@gmail.com>
2026-04-01 13:55:19 -0400

    Add Create Booking

--- src/test/java/TesteBasico.java
+++ src/test/java/TesteBasico.java
@@ -1,9 +1,19 @@
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class TesteBasico {
+
+    public String lerJson(String caminhoArquivo) throws IOException{
+        return new String(Files.readAllBytes(Paths.get(caminhoArquivo)));
+    }
 
     // Define um método de teste
     @Test
@@ -21,6 +31,43 @@ public class TesteBasico {
                 .log().all();    // Loga no console todos os detalhes da resposta (body, headers, etc.)
     }
 
+    @Test
+    public void testWithId(){
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
 
+        given()
+                .header("Accept", "application/json")
+                .when()
+                .get("/booking/3425")
+                .then()
+                .statusCode(200)
+                .body("firstname", equalTo("Thiago"))
+                .body("lastname", equalTo("Torres"))
+                .body("totalprice", equalTo(666))
+                .body("depositpaid", is(true))
+                .body("bookingdates.checkin", equalTo("2018-01-01"))
+                .body("bookingdates.checkout", equalTo("2019-01-01"))
+                .body("additionalneeds", equalTo("Breakfast"));
+    }
 
+@Test
+public void createBooking() throws IOException {
+    RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+    String jsonBody = lerJson("src/test/resources/payloads/reserva.json");
+
+    given()
+            .header("Content-Type", "application/json")
+            .body(jsonBody)
+            .when()
+            .post("/booking")
+            .then()
+            .statusCode(200)
+            .body("booking.firstname", equalTo("Thi"))
+            .body("booking.lastname", equalTo("Thi"))
+            .body("booking.totalprice", equalTo(666))
+            .body("booking.depositpaid", is(true))
+            .body("booking.bookingdates.checkin", equalTo("2025-01-01"))
+            .body("booking.bookingdates.checkout", equalTo("2026-01-01"))
+            .body("booking.additionalneeds", equalTo("Breakfast"));
+    }
 }
